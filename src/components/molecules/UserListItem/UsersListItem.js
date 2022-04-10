c08f67de19e03cdde7f10f48c5705799aaa5e322
Mateusz Krystkowicz <krissie515@gmail.com>
2022-04-10 20:38:47 +0200

    move styled components to separated files

--- src/components/molecules/UserListItem/UsersListItem.js
+++ src/components/molecules/UserListItem/UsersListItem.js
@@ -1,22 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
 import Button from 'components/atoms/Button/Button';
-
-const Wrapper = styled.li`
-  display: flex;
-  align-items: center;
-  position: relative;
-
-  &:not(:last-child)::after {
-    content: '';
-    position: absolute;
-    bottom: 0;
-    width: 100%;
-    height: 1px;
-    background-color: lightgray;
-  }
-`;
+import { Wrapper } from './UserListItem.styles';
 
 const UsersListItem = ({ userData: { name, average, attendance = '0%' } }) => (
   <Wrapper>
--- src/views/Root.js
+++ src/views/Root.js
@@ -1,16 +1,8 @@
 import UsersList from 'components/organisms/UsersList/UsersList';
-import styled, { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from 'assets/styles/GlobalStyles';
 import { theme } from 'assets/styles/theme';
-
-const Wrapper = styled.div`
-  background-color: ${({ theme }) => theme.colors.lightGrey};
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100vh;
-`;
+import { Wrapper } from './Root.styles';
 
 const Root = () => (
   <ThemeProvider theme={theme}>
